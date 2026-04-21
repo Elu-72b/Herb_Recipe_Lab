1669d568e44d3d057d6cf9d39f2a263f9c4d5511
Elu-72b <eriloach0339vialattea@gmail.com>
2026-04-22 01:25:29 +0900

    refactor:ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,30 +1,15 @@
 Rails.application.routes.draw do
-  get "herbs/index"
-  get "herbs/show"
-  get "herbs/new"
-  get "herbs/create"
-  get "herbs/edit"
-  get "herbs/update"
-  get "herbs/destroy"
   devise_for :users
-
-  # 1. ルートパス（/）の設定
-  # 未ログインなら static_pages#top（ログインフォーム付き）を表示し、
-  # ログイン済みなら Controller 側で home へリダイレクトさせます。
-  root 'static_pages#top'
 
   resources :herbs
 
-  # 2. 新規登録画面のURLをシンプルにする（任意）
-  # /users/sign_up ではなく /signup でアクセスしたい場合に残します。
+  root 'static_pages#top'
+
   devise_scope :user do
     get '/signup', to: 'devise/registrations#new'
   end
 
-  # 3. 認証後のメイン画面
   get 'home', to: 'static_pages#home'
 
   get "up" => "rails/health#show", as: :rails_health_check
-  get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 end
