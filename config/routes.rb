9e3dbfed4a814ca96a3c5ce6d6a9f4e90a284fc1
Elu-72b <eriloach0339vialattea@gmail.com>
2026-04-23 01:01:06 +0900

    Merge conflict resolved

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  # 1. ルートパス（/）の設定
   # 未ログインなら static_pages#top（ログインフォーム付き）を表示し、
   # ログイン済みなら Controller 側で home へリダイレクトさせます。
   root 'static_pages#top'
@@ -10,8 +9,6 @@ Rails.application.routes.draw do
   resources :recipes do
     resources :drinking_logs, only: [:new, :create, :show, :edit, :update]
   end
-  # 2. 新規登録画面のURLをシンプルにする（任意）
-  # /users/sign_up ではなく /signup でアクセスしたい場合に残します。
   devise_scope :user do
     get '/signup', to: 'devise/registrations#new'
   end
