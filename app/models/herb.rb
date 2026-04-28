3a83de7a51ebce6e1c6d32fde96b2dbd8fdbbd87
Elu-72b <eriloach0339vialattea@gmail.com>
2026-04-28 19:13:40 +0900

    fix: ハーブ図鑑の削除機能修正

--- app/models/herb.rb
+++ app/models/herb.rb
@@ -10,6 +10,8 @@ class Herb < ApplicationRecord
   has_many :herb_caution_tags, dependent: :destroy
   has_many :caution_tags, through: :herb_caution_tags
 
+  has_many :recipe_herbs, dependent: :destroy
+
   ACCEPTED_CONTENT_TYPES = %w[image/jpeg image/png image/gif image/webp].freeze
 
   validates :name, presence: true
