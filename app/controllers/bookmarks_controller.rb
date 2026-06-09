db0a6b2b1f3f9122b0f6f1bb97670e1d1752f183
Elu-72b <eriloach0339vialattea@gmail.com>
2026-06-09 23:10:07 +0900

    feat: ブックマーク登録・解除・一覧アクションを実装

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -2,5 +2,31 @@ class BookmarksController < ApplicationController
   before_action :require_login_with_alert
 
   def index
+    @bookmarks = current_user.bookmarks.includes(recipe: [:drinking_log, :user, recipe_herbs: :herb]).order(created_at: :desc)
+    # N+1対策: パーシャルで使う bookmarked_recipe_ids を一括取得
+    @bookmarked_recipe_ids = current_user.bookmarks.pluck(:recipe_id).to_set
+  end
+
+  def create
+    @recipe = Recipe.find(params[:recipe_id])
+    @bookmark = current_user.bookmarks.find_or_initialize_by(recipe: @recipe)
+    @bookmark.save
+    render_bookmark_turbo
+  end
+
+  def destroy
+    @bookmark = current_user.bookmarks.find(params[:id])
+    @recipe = @bookmark.recipe
+    @bookmark.destroy
+    render_bookmark_turbo
+  end
+
+  private
+
+  def render_bookmark_turbo
+    respond_to do |format|
+      format.turbo_stream
+      format.html { redirect_back fallback_location: recipe_path(@recipe) }
+    end
   end
 end
