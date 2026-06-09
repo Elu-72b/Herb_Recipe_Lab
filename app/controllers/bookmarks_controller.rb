067e1416a8ed4b2f66ef0d6f20fc62c7d44f3103
Elu-72b <eriloach0339vialattea@gmail.com>
2026-06-10 03:17:21 +0900

    refactor: ブックマークコントローラにタブ・ページネーション対応を追加、不要変数を削除

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -2,9 +2,25 @@ class BookmarksController < ApplicationController
   before_action :require_login_with_alert
 
   def index
-    @bookmarks = current_user.bookmarks.includes(recipe: [:drinking_log, :user, recipe_herbs: :herb]).order(created_at: :desc)
-    # N+1対策: パーシャルで使う bookmarked_recipe_ids を一括取得
-    @bookmarked_recipe_ids = current_user.bookmarks.pluck(:recipe_id).to_set
+    @active_tab = params[:tab] == "my" ? "my" : "public"
+
+    if @active_tab == "public"
+      # 他人がブックマークしたレシピ（自分以外のレシピ）
+      @bookmarks = current_user.bookmarks
+                              .joins(:recipe)
+                              .where.not(recipes: { user_id: current_user.id })
+                              .includes(recipe: [:drinking_log, :user, recipe_herbs: :herb])
+                              .order(created_at: :desc)
+                              .page(params[:page]).per(10)
+    else
+      # 自分のレシピのブックマーク
+      @bookmarks = current_user.bookmarks
+                              .joins(:recipe)
+                              .where(recipes: { user_id: current_user.id })
+                              .includes(recipe: [:drinking_log, :user, recipe_herbs: :herb])
+                              .order(created_at: :desc)
+                              .page(params[:my_page]).per(10)
+    end
   end
 
   def create
