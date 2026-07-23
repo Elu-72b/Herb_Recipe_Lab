0f31260310f90f1d48fff2bc60b791384e2ae5c2
エリュ-72b <eriloach0339vialattea@gmail.com>
2026-07-23 16:17:12 +0900

    feat: ログイン・新規登録画面にGoogleボタンを追加

--- app/javascript/controllers/terms_agreement_controller.js
+++ app/javascript/controllers/terms_agreement_controller.js
@@ -1,6 +1,7 @@
 import { Controller } from "@hotwired/stimulus";
 
 // 同意チェックが入るまで送信ボタンを非活性にする
+// メール登録と Google 新規登録の両方を対象にするため submitTargets（複数）で扱う
 export default class extends Controller {
   static targets = ["checkbox", "submit"];
 
@@ -9,6 +10,9 @@ export default class extends Controller {
   }
 
   toggle() {
-    this.submitTarget.disabled = !this.checkboxTarget.checked;
+    const agreed = this.checkboxTarget.checked;
+    this.submitTargets.forEach((button) => {
+      button.disabled = !agreed;
+    });
   }
 }
